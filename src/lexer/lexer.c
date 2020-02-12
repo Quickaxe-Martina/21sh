6e0f62e04542f59ecb3d7abb39e67d46eb974c68
petruha2001 <49908075+petruha2001@users.noreply.github.com>
2020-02-12 20:21:53 +0300

    Update lexer.c
    + ЭЭ

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -50,6 +50,11 @@ int				dop_lexer2(t_dop_str *tmp, char *line)
 
 int				dop_lexer1(t_dop_str *tmp, char *line)
 {
+	if (word_size(line + tmp->i_c) == -2)
+	{
+		tmp->i_c += 2;
+		return (0);
+	}
 	tmp->tmp_c = ft_strsub(line, tmp->i_c +
 	(ispar(line[tmp->i_c]) == 1 ? 1 : 0), word_size(line + tmp->i_c));
 	if (tmp->tail_c != NULL && tmp->tail_c->operator_type > 2)
